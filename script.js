c6d4b5b08d32565ebf148fc90d9168c596b6c2ff
estudiante-dm <estudiante2684@stafbarcelona.com>
2025-12-06 18:19:02 +0100

    botones eventos

--- script.js
+++ script.js
@@ -1,23 +1,24 @@
-
 const chatContainer = document.querySelector(".chat_container");
 const btnTimes = document.getElementById("times");
 const btnReset = document.getElementById("reset");
 const btnIconChat = document.getElementById("iconChat");
-const btnContainer = document.querySelector('.btn_container');
+const btnContainer = document.querySelector(".btn_container");
+const btnPresencial = document.getElementById("btnPresencial");
+const btnVirtual = document.getElementById("btnVirtual");
+const btnTele = document.getElementById("btnTele");
 /***********************************************************************************************************************************************************************************
  *********************MENU ICONO ***************************
  *********************************************************************************************************************************************************
  */
 function showChat() {
-	chatContainer.classList.toggle('menu');
+	chatContainer.classList.toggle("menu");
 	scrollToBottom();
 }
 
 btnIconChat.addEventListener("click", showChat);
 btnTimes.addEventListener("click", showChat);
-btnReset.addEventListener("click", event=> {
-	event.preventDefault()
-	inicial()
+btnReset.addEventListener("click", () => {
+	inicial();
 });
 
 // Con esta funcion, forzaremos el texto abajo.
@@ -30,30 +31,80 @@ function scrollToBottom() {
  **********************************************************************************BOTONES DE SELECCION DE CURSO
  ***********************************************************************************/
 function inicial() {
-	btnContainer.style.display = 'none';
+	btnContainer.style.display = "none";
 
-	const mensajeBot = document.querySelectorAll('.mensaje_bot');
-	mensajeBot.forEach((e,i)=> {
-		e.style.display = 'none'
+	const mensajeBot = document.querySelectorAll(".mensaje_bot");
+	mensajeBot.forEach((e, i) => {
+		e.style.display = "none";
 		setTimeout(() => {
-			e.style.display = 'inline-flex';
+			e.style.display = "inline-flex";
 			mostrarBtn();
-		}, (i+1)*1000);
+		}, (i + 1) * 1000);
 	});
+	scrollToBottom();
 }
 inicial();
 
 function mostrarBtn() {
-	setTimeout(()=> {
-		btnContainer.style.display = 'flex';
-	},2000);
-	btnContainer.style.display = 'none';
+	setTimeout(() => {
+		btnContainer.style.display = "flex";
+	}, 2000);
+	btnContainer.style.display = "none";
+	btnOpcion(e);
+	scrollToBottom();
 }
 
-function btnOpcion() {
-	null
-}
+/**
+ * Evento click de los botones
+ */
 
+btnPresencial.addEventListener("click", () => {
+	const chat = document.querySelector(".chat");
+	const mensajeUserDiv = document.createElement("div");
+	mensajeUserDiv.classList.add("mensaje_user");
+
+	const burbuja = document.createElement("div");
+	burbuja.classList.add("user_burbuja");
+	burbuja.textContent = btnPresencial.textContent;
+
+	mensajeUserDiv.appendChild(burbuja);
+	chat.appendChild(mensajeUserDiv);
+
+	btnContainer.style.display = "none";
+	scrollToBottom();
+});
+
+btnVirtual.addEventListener("click", () => {
+	const chat = document.querySelector(".chat");
+	const mensajeUserDiv = document.createElement("div");
+	mensajeUserDiv.classList.add("mensaje_user");
+
+	const burbuja = document.createElement("div");
+	burbuja.classList.add("user_burbuja");
+	burbuja.textContent = btnVirtual.textContent;
+
+	mensajeUserDiv.appendChild(burbuja);
+	chat.appendChild(mensajeUserDiv);
+
+	btnContainer.style.display = "none";
+	scrollToBottom();
+});
+
+btnTele.addEventListener("click", () => {
+	const chat = document.querySelector(".chat");
+	const mensajeUserDiv = document.createElement("div");
+	mensajeUserDiv.classList.add("mensaje_user");
+
+	const burbuja = document.createElement("div");
+	burbuja.classList.add("user_burbuja");
+	burbuja.textContent = btnTele.textContent;
+
+	mensajeUserDiv.appendChild(burbuja);
+	chat.appendChild(mensajeUserDiv);
+
+	btnContainer.style.display = "none";
+	scrollToBottom();
+});
 // function showChat() {
 // 	if (!visible) {
 // 		chatContainer.style.display = "flex";
@@ -63,11 +114,6 @@ function btnOpcion() {
 // 	}
 // 	visible = !visible;
 // }
-
-
-
-
-
 
 // const container = document.querySelector(".chat");
 // // BOT
